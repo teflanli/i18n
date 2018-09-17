e5d4d57d39ee655cf8a81b2a18fa8b5ef1557ee1
Turker Eflanli <teflanli@campusmgmt.com>
2018-09-17 09:58:43 -0400

    Add compilation to JSON

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
 const pot = require('./routes/pot');
+const compile = require('./routes/compile');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/pot', pot);
+app.use('/compile', compile);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
